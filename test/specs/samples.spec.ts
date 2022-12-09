3e6e8d1fbcfaec1d3f66c067fcfae416410d3c2c
duongnguyen-2711 <nvdd2711@gmail.com>
2022-12-09 15:50:56 +0700

    add test

---
+++ test/specs/samples.spec.ts
@@ -0,0 +1,36 @@
+/**
+ * Licensed under the MIT License.
+ */
+
+// import HomePage from "../pageobjects/HomePage.js";
+// import AlarmsPage from "../pageobjects/AlarmsPage.js";
+import TimerPage from "../pageobjects/TimerPage";
+// import AddTimerPage from "../pageobjects/AddTimerPage.js";
+// import SettingsPage from "../pageobjects/SettingsPage.js";
+
+describe("Samples", () => {
+  beforeEach( () => {
+
+  });
+
+  it("Goto AddTimer Page", async () => {
+    console.log("TESTTTTTTT")
+    // await TimerPage.isPageLoaded()
+    // TimerPage.waitForPageLoaded();
+    //
+    // TimerPage.gotoAddTimerPage();
+    // AddTimerPage.waitForPageLoaded();
+    //
+    // AddTimerPage.cancelAndGotoHomePage();
+  })
+
+  // it("Goto AlarmPage", () => {
+  //   HomePage.gotoAlarmsPage();
+  //   AlarmsPage.waitForPageLoaded();
+  // });
+  //
+  // it("Goto Settings", () => {
+  //   HomePage.gotoSettingsPage();
+  //   SettingsPage.waitForPageLoaded();
+  // });
+});
